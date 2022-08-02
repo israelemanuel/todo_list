e8819972030994b717bf9d5eaf95cda0d21ee4d4
israelemanuel <52048249+israelemanuel@users.noreply.github.com>
2022-08-01 21:41:47 -0300

    Adição de lista de tarefas

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,49 +1,77 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  TodoListPage({Key? key}) : super(key: key);
-
-  final TextEditingController emailController = TextEditingController();
+  const TodoListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.all(16),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: emailController,
-                onChanged: onChanged,
-                onSubmitted: onSubmited,
-                decoration: const InputDecoration(
-                  labelText: "E-mail",
-                ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Adicione uma tarefa",
+                        hintText: "Estudar Flutter",
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      //primary: Colors.cyan,
+                      primary: const Color(0xff00d7f3),
+                      //fixedSize: Size(100, 200),
+                      padding: const EdgeInsets.all(15),
+                    ),
+                    child: Icon(
+                      Icons.add,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
-               ElevatedButton(
-                onPressed: login,
-                child: const Text("Entrar"),
+              SizedBox(height: 8),
+              ListView(
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    color: Colors.red,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                    height: 50,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 50,
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                children: [
+                  Expanded(
+                    child: Text("Voê possui 0 tarefas pendentes"),
+                  ),
+                  SizedBox(width: 8),
+                  ElevatedButton(onPressed: () {}, child: Text("Limpar Tudo"))
+                ],
               )
             ],
           ),
         ),
       ),
     );
-  }
-
-  void login(){
-    String text = emailController.text;
-    print(text);
-    emailController.clear();
-  }
-
-  void onChanged(String text){
-    //print(text);
-  }
-
-  void onSubmited(String text){
-    print(text);
   }
 }
